02f0f5b24c47dafacf10cc334a47459152f8e354
Yoonjung Yim <yoonjung623@naver.com>
2023-03-22 22:05:34 +0900

    Feat : MainApplication

---
+++ matjib-back/src/main/java/matjibback/MatjibBackApplication.java
@@ -0,0 +1,13 @@
+package matjibback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatjibBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatjibBackApplication.class, args);
+	}
+
+}
