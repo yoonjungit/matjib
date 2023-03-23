64387713210110fc61b44855c47ecb1207e357f4
zeehoonee <qlscmgns@gmail.com>
2023-03-24 00:18:39 +0900

    feat : 네이버 크롤링 결과 프린트 추가함.
    윤정님이 올린 네이버크롤러 추가.

--- matjib-back/src/main/java/matjibback/MatjibBackApplication.java
+++ matjib-back/src/main/java/matjibback/MatjibBackApplication.java
@@ -1,6 +1,5 @@
 package matjibback;
 
-import org.checkerframework.checker.units.qual.K;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,11 +9,14 @@ public class MatjibBackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MatjibBackApplication.class, args);
 
-		GoogleCrawling google = new GoogleCrawling();
+		GoogleCrawler google = new GoogleCrawler();
 		google.printGoogleScore();
 
-		KakaoCrawling kakao = new KakaoCrawling();
+		KakaoCrawler kakao = new KakaoCrawler();
 		kakao.printKakaoScore();
+
+		NaverCrawler naver = new NaverCrawler();
+		naver.printNaverScore();
 	}
 
 }
