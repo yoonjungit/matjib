6a62fb080ce16a12cd449553e107fffc433a7bee
Yoonjung Yim <yoonjung623@naver.com>
2023-04-02 02:10:24 +0900

    style : 로그인 인증 중 화면(callback) 스피너 추가

--- matjib-front/src/pages/callback.vue
+++ matjib-front/src/pages/callback.vue
@@ -1,6 +1,29 @@
 <template>
-  <div>
-
+  <div class="spinner">
+    <div class="spinner-grow text-primary" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <div class="spinner-grow text-secondary" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <div class="spinner-grow text-success" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <div class="spinner-grow text-danger" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <div class="spinner-grow text-warning" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <div class="spinner-grow text-info" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <div class="spinner-grow text-light" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <div class="spinner-grow text-dark" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
   </div>
 </template>
 
@@ -56,5 +79,7 @@ export default {
 </script>
 
 <style>
-
+.spinner{
+  margin:200px
+}
 </style>
