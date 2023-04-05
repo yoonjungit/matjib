95f5ae6821dbea171333d90c1da4feb27cef390d
zeehoonee <qlscmgns@gmail.com>
2023-04-05 11:17:26 +0900

    style : infowindow 디자인

--- matjib-front/src/components/Map.vue
+++ matjib-front/src/components/Map.vue
@@ -1,10 +1,42 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Codespaces
+Marketplace
+Explore
+
+@zeehoonee
+yoonjungit
+/
+matjib
+Public
+Fork your own copy of yoonjungit/matjib
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+matjib/matjib-front/src/components/Map.vue
+@zeehoonee
+zeehoonee style : 인포윈도우 디자인, 지도 div 화면 꽉채우기, 검색창 지도 위에 올리기
+…
+Latest commit 231d9d3 2 hours ago
+History
+1 contributor
+108 lines (96 sloc)  4.47 KB
+
+
 <template>
-    <div id="map">
-      <div class="search">
-        <input type="text" name="searchIndex" onclick="this.value ='';" value="식당명을 입력하세요.">
-        <button type="submit">검색</button>
-      </div>
+  <div id="map">
+    <div class="search">
+      <input type="text" name="searchIndex" onclick="this.value ='';" value="식당명을 입력하세요.">
+      <button type="submit">검색</button>
     </div>
+  </div>
 </template>
 
 <script>
@@ -16,8 +48,9 @@ export default {
       marker: null,
       infowindow: null,
       zoomControl: null,
+      coords: null,
       geocoder: null,
-      content: '<div class="wrap"><div class="info"><div class="title">식당이름</div><div class="buttons"><button>리스트</button><button>북마크</button></div><div class="body"><div class="address">주소</div><div class="naver">네이버 별점/갯수</div><div class="kakao">카카오별점/갯수</div><div class="google">구글별점/갯수</div></div></div></div></div></div>' // 인포윈도우에 표시할 내용
+      content: '<div class="wrap"><div class="info"><div class="title">식당이름<button id="bookmark">북마크</button></div><div class="body"><div class="address">주소</div><div class="naver"><img src="https://map.naver.com/v5/assets/icon/favicon-32x32.png" width="20px" height="20px"> 네이버 평점/갯수</div><div class="kakao"><img src="https://map.kakao.com/favicon.ico" width="20px" height="20px"> 카카오별점/갯수</div><div class="google"><img src="https://www.google.com/images/branding/product/ico/maps15_bnuw3a_32dp.ico" width="20px" height="20px"> 구글별점/갯수</div></div></div></div></div></div>' // 인포윈도우에 표시할 내용
     };
   },
   mounted() {
@@ -28,7 +61,7 @@ export default {
       /* global kakao */
       script.onload = () => kakao.maps.load(this.initMap);
       script.src =
-          "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=915cffed372954b7b44804ed422b9cf0&libraries=services,clusterer,drawing";
+          "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=" + process.env.VUE_APP_KAKAO_MAP_APP_KEY + "&libraries=services,clusterer,drawing";
       document.head.appendChild(script);
     }
   },
@@ -36,41 +69,50 @@ export default {
     initMap() {
       const container = document.getElementById("map");
       const options = {
-        center: new kakao.maps.LatLng(38.04007, 126.92658), // 지도의 중심좌표벨
+        center: new kakao.maps.LatLng(38.04007, 126.92658), // 지도의 중심좌표
         level: 4, //지도의 확대 레벨
       };
-
       //지도 객체를 등록합니다.
       //지도 객체는 반응형 관리 대상이 아니므로 initMap에서 선언합니다.
       this.map = new kakao.maps.Map(container, options);
+      // 주소-좌표 변환 객체를 생성합니다
       this.geocoder = new kakao.maps.services.Geocoder();
-
-
       // 지도에 확대 축소 컨트롤을 생성한다.
       this.zoomControl = new kakao.maps.ZoomControl();
-
       // 지도의 우측에 확대 축소 컨트롤을 추가한다.
       this.map.addControl(this.zoomControl, kakao.maps.ControlPosition.RIGHT);
 
-      // 지도에 마커를 생성하고 표시한다
-      this.marker = new kakao.maps.Marker({
-        position: new kakao.maps.LatLng(38.04007, 126.92658), // 마커의 좌표
-        map: this.map // 마커를 표시할 지도 객체
-      });
+      // 주소로 좌표를 검색합니다
+      this.geocoder.addressSearch('강원도 춘천시 안마산로 133', function (result, status) {
+        // 정상적으로 검색이 완료됐으면
+        if (status === kakao.maps.services.Status.OK) {
 
-      // 마커 위에 표시할 인포윈도우를 생성한다
-      this.infowindow = new kakao.maps.InfoWindow({
-        content: this.content
-      });
+          this.coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
-      // 마커에 클릭 이벤트를 등록한다 (우클릭 : rightclick)
-      kakao.maps.event.addListener(this.marker, 'click', function () {
-        this.infowindow.open(this.map, this.marker);
-      }.bind(this));
+          // 결과값으로 받은 위치를 마커로 표시합니다
+          this.marker = new kakao.maps.Marker({
+            map: this.map,
+            position: this.coords
+          })
 
-      // 지도에 클릭 이벤트를 등록한다.
-      kakao.maps.event.addListener(this.map, 'click', function () {
-        this.infowindow.close();
+          // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+          this.map.setCenter(this.coords);
+
+          // 마커 위에 표시할 인포윈도우를 생성한다
+          this.infowindow = new kakao.maps.InfoWindow({
+            content: this.content
+          });
+
+          // 마커에 클릭 이벤트를 등록한다 (우클릭 : rightclick)
+          kakao.maps.event.addListener(this.marker, 'click', function () {
+            this.infowindow.open(this.map, this.marker);
+          }.bind(this));
+
+          // 지도에 클릭 이벤트를 등록한다.
+          kakao.maps.event.addListener(this.map, 'click', function () {
+            this.infowindow.close();
+          }.bind(this));
+        }
       }.bind(this));
     },
   },
@@ -79,20 +121,56 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-.wrap {position: absolute;left: 0;bottom: 40px;width: 288px;height: 132px;margin-left: -144px;text-align: left;overflow: hidden;font-size: 12px;font-family: 'Malgun Gothic', dotum, '돋움', sans-serif;line-height: 1.5;}
-.wrap * {padding: 0;margin: 0;}
-.wrap .info {width: 286px;height: 120px;border-radius: 5px;border-bottom: 2px solid #ccc;border-right: 1px solid #ccc;overflow: hidden;background: #fff;}
-.wrap .info:nth-child(1) {border: 0;box-shadow: 0px 1px 2px #888;}
-.info .title {padding: 5px 0 0 10px;height: 30px;background: #eee;border-bottom: 1px solid #ddd;font-size: 18px;font-weight: bold;}
-.info .close {position: absolute;top: 10px;right: 10px;color: #888;width: 17px;height: 17px;background: url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/overlay_close.png');}
-.info .close:hover {cursor: pointer;}
-.info .body {position: relative;overflow: hidden;}
-.info .desc {position: relative;margin: 13px 0 0 90px;height: 75px;}
-.desc .ellipsis {overflow: hidden;text-overflow: ellipsis;white-space: nowrap;}
-.desc .jibun {font-size: 11px;color: #888;margin-top: -2px;}
-.info .img {position: absolute;top: 6px;left: 5px;width: 73px;height: 71px;border: 1px solid #ddd;color: #888;overflow: hidden;}
-.info:after {content: '';position: absolute;margin-left: -12px;left: 50%;bottom: 0;width: 22px;height: 12px;background: url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png')}
-.info .link {color: #5085BB;}
+
+#bookmark {
+  width: 70px;
+  float: right;
+  margin-right: 10px;
+  border: none;
+  font-size: 17px;
+  border-radius: 5px;
+}
+
+.wrap {
+  width: 288px;
+  height: 150px;
+}
+
+.wrap .info {
+  width: 286px;
+  height: 150px;
+  border-radius: 0;
+  border-bottom: 2px solid #ccc;
+  border-right: 1px solid #ccc;
+  overflow: hidden;
+  background: #fff;
+}
+
+.wrap .info:nth-child(1) {
+  border: 0;
+  box-shadow: 0 1px 2px #888;
+}
+
+.info .title {
+  padding: 5px 0 0 10px;
+  height: 40px;
+  background: #E74C3C;
+  border-bottom: 1px solid #ddd;
+  color: white;
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.info .body {
+  margin-left: 10px;
+  position: relative;
+  overflow: hidden;
+}
+
+.address {
+  font-size: 17px;
+  color: #E74C3C;
+}
 
 .search {
   position: relative;
