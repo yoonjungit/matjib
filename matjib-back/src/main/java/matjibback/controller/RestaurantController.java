d3c91325c35c30b62d7e4ea8ac6667380cf49619
Yoonjung Yim <yoonjung623@naver.com>
2023-04-05 16:49:06 +0900

    feat : 현재 지도 중심위치 근방 맛집 리스트 검색 및 반환

--- matjib-back/src/main/java/matjibback/controller/RestaurantController.java
+++ matjib-back/src/main/java/matjibback/controller/RestaurantController.java
@@ -14,17 +14,31 @@ public class RestaurantController {
     @Autowired
     RestaurantRepository restaurantRepository;
 
-    @GetMapping("/matjib/getMap")
+    //전체 맛집 리스트 받아오기
+    @GetMapping("/matjib/restaurants/getMap")
     public List<Restaurant> getMap() {
         List<Restaurant> restaurants = restaurantRepository.findAll();
         return restaurants;
     }
 
-    @PostMapping("/matjib/find")
+    //검색어로 맛집 찾기
+    @PostMapping("/matjib/restaurants/search")
     public List<Restaurant> findMap(@RequestBody Map<String, String> params) {
-        System.out.println(params.get("resName"));
+        System.out.println("검색어 : " + params.get("resName"));
         List<Restaurant> restaurants = restaurantRepository.findRestaurantByResNameContaining(params.get("resName"));
-        System.out.println(restaurants.size());
+        System.out.println("검색결과 : " + restaurants.size() + "건");
+        return restaurants;
+    }
+
+    //현재 지도 중심위치 근방 맛집 보여주기
+    @PostMapping("/matjib/restaurants/show")
+    public List<Restaurant> showMap(@RequestBody Map<String, String> params) {
+        float latS= Float.parseFloat(String.valueOf(params.get("latS")));
+        float latE= Float.parseFloat(String.valueOf(params.get("latE")));
+        float longS= Float.parseFloat(String.valueOf(params.get("longS")));
+        float longE= Float.parseFloat(String.valueOf(params.get("longE")));
+        List<Restaurant> restaurants = restaurantRepository.findRestaurantByLatitudeBetweenAndLongitudeBetween(latS, latE, longS, longE);
+        System.out.println("검색결과 : "+ restaurants.size() +"건");
         return restaurants;
     }
 }
